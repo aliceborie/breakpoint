e9f46e39a29ee9f01a67dc7200f3ae769680d0ef
Alice Borie <aborie@andrew.cmu.edu>
2015-11-02 12:15:51 -0500

    Set up basic controllers and routes

---
+++ www/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('breakpoint.controllers', [])
+
+.controller('LandingCtrl', function($scope) {
+})
+
+.controller('BrowseCtrl', function($scope) {
+	// hardcoding these until we use Parse 
+	$scope.categories = [
+		{id: 1, name: 'Recipes', url: 'recipes'},
+		{id: 2, name: 'Lectures', url: 'lectures'},
+		{id: 3, name: 'Fix It Yourself', url: 'fix-it-yourself'},
+		{id: 4, name: 'Music', url: 'music'},
+	]
+})
+
+.controller('CategoryCtrl', function($scope, $stateParams) {
+	// get category name from params so can know which category 
+	var category = $stateParams.categoryName;
+	getCategory(category);
+
+	function getCategory(name) {
+		// hardcoding these until we use Parse 
+		categories = [
+			{id: 1, name: 'Recipes', url: 'recipes'},
+			{id: 2, name: 'Lectures', url: 'lectures'},
+			{id: 3, name: 'Fix It Yourself', url: 'fix-it-yourself'},
+			{id: 4, name: 'Music', url: 'music'},
+		]
+
+		categories.forEach( function(category) {
+			if (category.url == name) {
+				$scope.category = category;
+			}
+		})
+	}
+})
